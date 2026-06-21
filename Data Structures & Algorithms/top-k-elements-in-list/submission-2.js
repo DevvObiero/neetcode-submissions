89dc39ff3e04bbc0441ef6bd35f7cb398d7444ed
Paul Obiero <157304888+DevvObiero@users.noreply.github.com>
2026-06-21 17:28:36 +0300

    Add: top-k-elements-in-list - submission-2

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-2.js
@@ -0,0 +1,45 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {number[]}
+     */
+    topKFrequent(nums, k) {
+        const count  =  {};
+
+     for (const num of nums){
+count[num]  =   (count[num] || 0 ) +1;
+}
+const buckets =  Array(nums.length +1).fill().map(()=>[]);
+// 444557
+for(const num in count){
+    const freq =  count[num];
+    buckets[freq].push(Number(num))
+}
+const result = [];
+
+
+for(let i = buckets.length -1;i>= 0;i-- ){
+    for (const num of buckets[i] ){
+        result.push(num);
+        if(result.length === k ){
+            return result
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
