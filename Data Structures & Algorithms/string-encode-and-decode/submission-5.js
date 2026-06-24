1e28ff3f7e5792982cd3afbc6e729d18934baba7
Paul Obiero <157304888+DevvObiero@users.noreply.github.com>
2026-06-24 21:45:59 +0300

    Add: string-encode-and-decode - submission-5

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-5.js
@@ -0,0 +1,38 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @returns {string}
+     */
+    encode(strs) {
+      let res = ''
+      for(let s of strs){
+        res += s.length + "#" + s;
+      }
+      return res;
+    }
+
+    /**
+     * @param {string} str
+     * @returns {string[]}
+     */
+    decode(str) {
+let res = [];
+let  i = 0;
+ 
+
+while(i < str.length){
+  let j = i ;
+
+  while(str[j] !== "#"){
+    j++;
+  }
+  let length = Number(str.substring(i,j));
+  i = j+1;
+  let word = str.substring(i,i + length);
+  res.push(word);
+  i = i +length
+}
+return res
+
+    }
+}
